cb9eab8171b7d9f4c582c98c153d177eb11b95d3
jwe0516 <jwe0516@naver.com>
2026-01-05 17:09:53 +0900

    feat: 헤더 UI 구현

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,22 @@ import Logo from "./components/Logo";
 const App = () => {
   return (
     <div className="p-5">
-      <Logo iconSize={32} textSize="16px" spacing="mt-0.5 ml-1" />
+      <header>
+        <div className="flex justify-between">
+          <Logo iconSize={32} textSize="16px" spacing="mt-0.5 ml-1" />
+          <div className="flex gap-x-2">
+            <button className="bg-sl-white border border-sl-blue w-[65px] h-[30px] rounded-2xl font-medium">
+              기록
+            </button>
+            <button className="bg-sl-white border border-sl-blue w-[65px] h-[30px] rounded-2xl font-medium">
+              로그인
+            </button>
+            <button className="bg-sl-white border border-sl-blue w-[65px] h-[30px] rounded-2xl font-medium">
+              설정
+            </button>
+          </div>
+        </div>
+      </header>
     </div>
   );
 };
