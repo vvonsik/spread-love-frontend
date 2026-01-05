db5d46839905d7ea50815a3885b67a5d85d75989
harim lee <harimlee9506@gmail.com>
2026-01-06 04:15:58 +0900

    feat: 설정 탭 SettingLink 컴포넌트 구현

---
+++ src/components/SettingLink.jsx
@@ -0,0 +1,9 @@
+const SettingLink = ({ label }) => {
+  return (
+    <a href="#" className="block text-sl-blue text-lg mb-4 hover:underline">
+      {label}
+    </a>
+  );
+};
+
+export default SettingLink;
