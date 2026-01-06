045e56c09b8e484e5a03fd90edcebe869a836059
jwe0516 <jwe0516@naver.com>
2026-01-06 19:52:24 +0900

    feat: Button 컴포넌트에 textColor prop 추가

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,8 @@
-const Button = ({ children, bgColor, borderColor }) => {
+const Button = ({ children, bgColor, borderColor, textColor = "text-sl-black" }) => {
   return (
     <button
       type="button"
-      className={`w-[65px] h-[30px] ${bgColor} border ${borderColor} rounded-2xl font-medium`}
+      className={`w-[65px] h-[30px] ${bgColor} border ${borderColor} rounded-2xl font-medium ${textColor}`}
     >
       {children}
     </button>
