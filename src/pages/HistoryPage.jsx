2f250a71acd73f6ef15ec81e99e1a481039091f3
jwe0516 <jwe0516@naver.com>
2026-01-06 20:00:54 +0900

    chore: 스타일 컨벤션에 맞게 조정

--- src/pages/HistoryPage.jsx
+++ src/pages/HistoryPage.jsx
@@ -2,41 +2,35 @@ const HistoryPage = () => {
   return (
     <>
       <ul className="flex flex-col w-full gap-3">
-        <li className="flex w-full h-[40px] bg-sl-blue  justify-center items-center rounded-2xl text-sl-white text-base">
-          <a href="">2026년 1월 3일 기록</a>
+        <li className="flex justify-center items-center w-full h-10 bg-sl-blue rounded-2xl text-base text-sl-white">
+          <a href="/">2026년 1월 3일 기록</a>
         </li>
-        <li className="flex w-full h-[40px] bg-sl-blue  justify-center items-center rounded-2xl text-sl-white text-base">
-          <a href="">2026년 1월 4일 기록</a>
+        <li className="flex justify-center items-center w-full h-10 bg-sl-blue rounded-2xl text-base text-sl-white">
+          <a href="/">2026년 1월 3일 기록</a>
         </li>
-        <li className="flex w-full h-[40px] bg-sl-blue  justify-center items-center rounded-2xl text-sl-white text-base">
-          <a href="">2026년 1월 5일 기록</a>
+        <li className="flex justify-center items-center w-full h-10 bg-sl-blue rounded-2xl text-base text-sl-white">
+          <a href="/">2026년 1월 3일 기록</a>
         </li>
-        <li className="flex w-full h-[40px] bg-sl-blue  justify-center items-center rounded-2xl text-sl-white text-base">
-          <a href="">2026년 1월 5일 기록</a>
+        <li className="flex justify-center items-center w-full h-10 bg-sl-blue rounded-2xl text-base text-sl-white">
+          <a href="/">2026년 1월 3일 기록</a>
         </li>
-        <li className="flex w-full h-[40px] bg-sl-blue  justify-center items-center rounded-2xl text-sl-white text-base">
-          <a href="">2026년 1월 5일 기록</a>
+        <li className="flex justify-center items-center w-full h-10 bg-sl-blue rounded-2xl text-base text-sl-white">
+          <a href="/">2026년 1월 3일 기록</a>
         </li>
-        <li className="flex w-full h-[40px] bg-sl-blue  justify-center items-center rounded-2xl text-sl-white text-base">
-          <a href="">2026년 1월 5일 기록</a>
+        <li className="flex justify-center items-center w-full h-10 bg-sl-blue rounded-2xl text-base text-sl-white">
+          <a href="/">2026년 1월 3일 기록</a>
         </li>
-        <li className="flex w-full h-[40px] bg-sl-blue  justify-center items-center rounded-2xl text-sl-white text-base">
-          <a href="">2026년 1월 5일 기록</a>
+        <li className="flex justify-center items-center w-full h-10 bg-sl-blue rounded-2xl text-base text-sl-white">
+          <a href="/">2026년 1월 3일 기록</a>
         </li>
-        <li className="flex w-full h-[40px] bg-sl-blue  justify-center items-center rounded-2xl text-sl-white text-base">
-          <a href="">2026년 1월 5일 기록</a>
+        <li className="flex justify-center items-center w-full h-10 bg-sl-blue rounded-2xl text-base text-sl-white">
+          <a href="/">2026년 1월 3일 기록</a>
         </li>
-        <li className="flex w-full h-[40px] bg-sl-blue  justify-center items-center rounded-2xl text-sl-white text-base">
-          <a href="">2026년 1월 5일 기록</a>
+        <li className="flex justify-center items-center w-full h-10 bg-sl-blue rounded-2xl text-base text-sl-white">
+          <a href="/">2026년 1월 3일 기록</a>
         </li>
-        <li className="flex w-full h-[40px] bg-sl-blue  justify-center items-center rounded-2xl text-sl-white text-base">
-          <a href="">2026년 1월 5일 기록</a>
-        </li>
-        <li className="flex w-full h-[40px] bg-sl-blue  justify-center items-center rounded-2xl text-sl-white text-base">
-          <a href="">2026년 1월 5일 기록</a>
-        </li>
-        <li className="flex w-full h-[40px] bg-sl-blue  justify-center items-center rounded-2xl text-sl-white text-base">
-          <a href="">2026년 1월 5일 기록</a>
+        <li className="flex justify-center items-center w-full h-10 bg-sl-blue rounded-2xl text-base text-sl-white">
+          <a href="/">2026년 1월 3일 기록</a>
         </li>
       </ul>
     </>
