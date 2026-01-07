6723f8830f563c9993f004d40954190c90bca13b
jwe0516 <jwe0516@naver.com>
2026-01-07 17:46:56 +0900

    feat: LoadingSpinner 회전 애니메이션 추가

--- src/components/LoadingSpinner.jsx
+++ src/components/LoadingSpinner.jsx
@@ -1,9 +1,15 @@
 const LoadingSpinner = ({ message }) => {
   return (
     <div className="flex flex-col items-center gap-4">
-      <img src="/images/icons/spreadlove-loading-48.svg" alt="로딩 이미지" aria-hidden="true" />
+      <img
+        src="/images/icons/spreadlove-loading-48.svg"
+        alt="로딩 이미지"
+        aria-hidden="true"
+        className="animate-spin"
+      />
       <p className="text-[16px]">{message}</p>
     </div>
   );
 };
+
 export default LoadingSpinner;
