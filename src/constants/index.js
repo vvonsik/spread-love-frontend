b4051b0cae622be1a9e21555e2788122a8e06048
jwe0516 <jwe0516@naver.com>
2026-01-08 11:05:56 +0900

    chore: 테스트용 목업 데이터 추가

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,17 @@
 const SUMMARY_STATUS_FLOW = ["default", "loading", "result"];
+const TEST_DATA = {
+  YONHAP_NEWS: {
+    url: "https://www.yna.co.kr/view/AKR20231213165600007",
+    image: "/images/mocks/spreadlove-screenshot-01.png",
+  },
+  NAVER_SPORTS: {
+    url: "https://m.sports.naver.com/kbaseball/article/311/0001961081",
+    image: "/images/mocks/spreadlove-screenshot-02.png",
+  },
+  MUSINSA: {
+    url: "https://www.musinsa.com/products/5446082",
+    image: "/images/mocks/spreadlove-screenshot-03.png",
+  },
+};
 
-export { SUMMARY_STATUS_FLOW };
+export { SUMMARY_STATUS_FLOW, TEST_DATA };
