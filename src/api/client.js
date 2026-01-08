2974ade60dde2dd347879ce10a74a823a74cf353
jwe0516 <jwe0516@naver.com>
2026-01-08 15:24:58 +0900

    feat: 테스트용 인증 토큰 헤더 설정

--- src/api/client.js
+++ src/api/client.js
@@ -2,6 +2,9 @@ import ky from "ky";
 
 const api = ky.create({
   prefixUrl: import.meta.env.VITE_API_BASE_URL,
+  headers: {
+    Authorization: `Bearer ${import.meta.env.VITE_TEMP_AUTH_TOKEN}`,
+  },
 });
 
 export { api };
