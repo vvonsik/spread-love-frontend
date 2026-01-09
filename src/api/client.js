2f59accb9ad586c0c7c0955b74a562d02461a149
jwe0516 <jwe0516@naver.com>
2026-01-09 10:57:29 +0900

    chore: ky timeout 30초로 설정 추가

--- src/api/client.js
+++ src/api/client.js
@@ -2,6 +2,7 @@ import ky from "ky";
 
 const api = ky.create({
   prefixUrl: import.meta.env.VITE_API_BASE_URL,
+  timeout: 30000,
   headers: {
     Authorization: `Bearer ${import.meta.env.VITE_TEMP_AUTH_TOKEN}`,
   },
