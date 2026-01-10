178996a47ad28a19dfce1e9e04697d1003771646
harim lee <harimlee9506@gmail.com>
2026-01-10 21:30:55 +0900

    feat: 기록 버튼 /history 라우팅 연결

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router";
 import Logo from "./Logo";
 import Button from "./Button";
 
@@ -7,9 +8,11 @@ const Header = () => {
       <div className="flex justify-between">
         <Logo iconSize={32} textSize={16} spacing="mt-0.5 ml-1" />
         <div className="flex gap-x-2">
-          <Button bgColor="bg-sl-white" borderColor="border-sl-blue">
-            기록
-          </Button>
+          <Link to="/history">
+            <Button bgColor="bg-sl-white" borderColor="border-sl-blue">
+              기록
+            </Button>
+          </Link>
           <Button bgColor="bg-sl-white" borderColor="border-sl-blue">
             로그인
           </Button>
