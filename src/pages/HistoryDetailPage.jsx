ab55863e398d44e8272653d1fa86c8d1ef87049e
harim lee <harimlee9506@gmail.com>
2026-01-11 08:15:43 +0900

    feat: history 상세 페이지 라우트 설정

---
+++ src/pages/HistoryDetailPage.jsx
@@ -0,0 +1,10 @@
+const HistoryDetailPage = () => {
+  return (
+    <div className="flex flex-col items-center justify-center w-full h-full">
+      <h1 className="text-[32px]">히스토리 상세 페이지</h1>
+      <p>여기에 상세 정보가 표시됩니다.</p>
+    </div>
+  );
+};
+
+export default HistoryDetailPage;
--- src/router/index.jsx
+++ src/router/index.jsx
@@ -2,6 +2,7 @@ import { createMemoryRouter } from "react-router";
 import App from "../App";
 import SummaryPage from "../pages/SummaryPage";
 import HistoryPage from "../pages/HistoryPage";
+import HistoryDetailPage from "../pages/HistoryDetailPage";
 
 export const router = createMemoryRouter([
   {
@@ -10,6 +11,7 @@ export const router = createMemoryRouter([
     children: [
       { index: true, element: <SummaryPage /> },
       { path: "history", element: <HistoryPage /> },
+      { path: "history/:id", element: <HistoryDetailPage /> },
     ],
   },
 ]);
