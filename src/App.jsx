d383595854989b52e9b0f4187782c75d1a93729f
jwe0516 <jwe0516@naver.com>
2026-01-12 13:50:25 +0900

    refactor: summaryStatus 상태를 App으로 이동

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import { useEffect, useState } from "react";
 import { Outlet, useLocation } from "react-router";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import { SUMMARY_STATUS } from "./constants/index.js";
 
 const App = () => {
   const location = useLocation();
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [summaryStatus, setSummaryStatus] = useState(SUMMARY_STATUS.DEFAULT);
 
   useEffect(() => {
     const tempToken = import.meta.env.VITE_TEMP_AUTH_TOKEN;
@@ -30,11 +32,15 @@ const App = () => {
 
   return (
     <div className="flex flex-col h-screen gap-5 p-5">
-      <Header isLoggedIn={isLoggedIn} />
+      <Header isLoggedIn={isLoggedIn} summaryStatus={summaryStatus} />
       <main className="flex flex-col items-center gap-2 w-full h-full p-3 border border-sl-blue rounded-xl overflow-y-auto">
-        <Outlet context={{ isLoggedIn }} />
+        <Outlet context={{ isLoggedIn, summaryStatus, setSummaryStatus }} />
       </main>
-      <Footer isLoggedIn={isLoggedIn} currentPath={location.pathname} />
+      <Footer
+        isLoggedIn={isLoggedIn}
+        currentPath={location.pathname}
+        summaryStatus={summaryStatus}
+      />
     </div>
   );
 };
--- src/pages/SummaryPage.jsx
+++ src/pages/SummaryPage.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
+import { useOutletContext } from "react-router";
 import LoadingSpinner from "../components/LoadingSpinner";
-import { SUMMARY_STATUS, TEST_DATA } from "../constants/Index";
+import { SUMMARY_STATUS, TEST_DATA } from "../constants/index";
 
 const blobToBase64 = (blob) => {
   return new Promise((resolve) => {
@@ -12,11 +13,11 @@ const blobToBase64 = (blob) => {
 };
 
 const SummaryPage = () => {
-  const [status, setStatus] = useState(SUMMARY_STATUS.DEFAULT);
+  const { summaryStatus, setSummaryStatus } = useOutletContext();
   const [summaryData, setSummaryData] = useState(null);
 
   const handleSummaryClick = async () => {
-    setStatus(SUMMARY_STATUS.LOADING);
+    setSummaryStatus(SUMMARY_STATUS.LOADING);
 
     const testItem = TEST_DATA.MUSINSA;
     const response = await fetch(testItem.image);
@@ -34,17 +35,17 @@ const SummaryPage = () => {
       (response) => {
         if (response?.success) {
           setSummaryData(response.data);
-          setStatus(SUMMARY_STATUS.RESULT);
+          setSummaryStatus(SUMMARY_STATUS.RESULT);
         } else {
           console.error("요약 실패:", response?.error);
-          setStatus(SUMMARY_STATUS.DEFAULT);
+          setSummaryStatus(SUMMARY_STATUS.DEFAULT);
         }
       },
     );
   };
   return (
     <div className="flex flex-col items-center justify-center w-full h-full">
-      {status === SUMMARY_STATUS.DEFAULT && (
+      {summaryStatus === SUMMARY_STATUS.DEFAULT && (
         <button
           type="button"
           onClick={handleSummaryClick}
@@ -54,8 +55,10 @@ const SummaryPage = () => {
           <span>이 페이지 요약하기</span>
         </button>
       )}
-      {status === SUMMARY_STATUS.LOADING && <LoadingSpinner message={"페이지를 요약중입니다..."} />}
-      {status === SUMMARY_STATUS.RESULT && summaryData && (
+      {summaryStatus === SUMMARY_STATUS.LOADING && (
+        <LoadingSpinner message={"페이지를 요약중입니다..."} />
+      )}
+      {summaryStatus === SUMMARY_STATUS.RESULT && summaryData && (
         <div className="flex flex-col gap-4">
           <h1 className="text-[32px]">{summaryData.title}</h1>
           <p className="text-2xl">{summaryData.summary}</p>
