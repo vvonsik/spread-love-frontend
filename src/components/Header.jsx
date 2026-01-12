80bf573e8bc10d15f5dd6922c3c72a49bf848084
jwe0516 <jwe0516@naver.com>
2026-01-12 13:53:11 +0900

    refactor: Header 컴포넌트 조건부 렌더링 구현

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,30 @@
 import Logo from "./Logo";
 import Button from "./Button";
+import { SUMMARY_STATUS } from "../constants/index.js";
 
-const Header = () => {
+const Header = ({ isLoggedIn, summaryStatus }) => {
+  const isLoading = summaryStatus === SUMMARY_STATUS.LOADING;
+
   return (
     <header>
       <div className="flex justify-between">
         <Logo iconSize={32} textSize={16} spacing="mt-0.5 ml-1" />
         <div className="flex gap-x-2">
-          <Button to="/history" bgColor="bg-sl-white" borderColor="border-sl-blue">
-            기록
-          </Button>
-          <Button bgColor="bg-sl-white" borderColor="border-sl-blue">
-            로그인
-          </Button>
+          {!isLoading && isLoggedIn && (
+            <Button to="/history" bgColor="bg-sl-white" borderColor="border-sl-blue">
+              기록
+            </Button>
+          )}
+          {!isLoading && !isLoggedIn && (
+            <Button bgColor="bg-sl-white" borderColor="border-sl-blue">
+              로그인
+            </Button>
+          )}
+          {!isLoading && isLoggedIn && (
+            <Button bgColor="bg-sl-white" borderColor="border-sl-blue">
+              로그아웃
+            </Button>
+          )}
           <Button bgColor="bg-sl-white" borderColor="border-sl-blue">
             설정
           </Button>
