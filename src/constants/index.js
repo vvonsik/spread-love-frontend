2ba0dedb8d8622427bc6921602535d54f4e150e1
jwe0516 <jwe0516@naver.com>
2026-02-10 12:54:40 +0900

    chore: 미사용 파일, 상수, 패키지 삭제

--- src/constants/index.js
+++ src/constants/index.js
@@ -28,21 +28,6 @@ const DEFAULT_SETTINGS = {
   persona: "default",
 };
 
-const TEST_DATA = {
-  YONHAP_NEWS: {
-    url: "https://www.yna.co.kr/view/AKR20231213165600007",
-    image: "/images/mocks/spreadlove-screenshot-01.png",
-  },
-  NAVER_SPORTS: {
-    url: "https://m.sports.naver.com/kbaseball/article/311/0001961081",
-    image: "/images/mocks/spreadlove-screenshot-02.png",
-  },
-  MUSINSA: {
-    url: "https://www.musinsa.com/products/5446082",
-    image: "/images/mocks/spreadlove-screenshot-03.png",
-  },
-};
-
 const PAGINATION = {
   PAGE_SIZE: 12,
   GROUP_SIZE: 5,
@@ -74,7 +59,6 @@ export {
   LENGTH_OPTIONS,
   PERSONA_OPTIONS,
   DEFAULT_SETTINGS,
-  TEST_DATA,
   PAGINATION,
   TOKEN_PREFIX,
   ERROR_CODE,
