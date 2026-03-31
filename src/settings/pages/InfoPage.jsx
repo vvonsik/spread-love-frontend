dfcf26a9c5c9ae755f6c7a9fb65007a97dfa754b
jwe0516 <jwe0516@naver.com>
2026-03-31 20:52:09 +0900

    chore: 정보 링크 수정

--- src/settings/pages/InfoPage.jsx
+++ src/settings/pages/InfoPage.jsx
@@ -4,8 +4,14 @@ import Link from "../components/Link.jsx";
 const InfoPage = () => {
   return (
     <TabLayout title="정보">
-      <Link label="사용 방법" url="https://github.com/SpreadLoveProject/spread-love-frontend" />
-      <Link label="버그 제보" url="https://github.com/SpreadLoveProject/spread-love-frontend" />
+      <Link
+        label="사용 방법"
+        url="https://chromewebstore.google.com/detail/spread-love/dmklbfcmddccklcghkijhgglpgmfedlh?hl=ko&authuser=3"
+      />
+      <Link
+        label="버그 제보"
+        url="https://github.com/Team-SpreadLove/spread-love-frontend/issues"
+      />
       <Link label="Github" url="https://github.com/SpreadLoveProject/spread-love-frontend" />
     </TabLayout>
   );
